906b60fd7bb54381a474f611a35264444a1d3355
Gustavo Lara <ing.glaragomez@gmail.com>
2024-12-14 20:25:36 -0700

    Added more pokemons

--- selection-screen.lua
+++ selection-screen.lua
@@ -2,7 +2,7 @@ local resolutionManager = require 'resolution-manager'
 
 local selectionScreen = {
   pokemonItems = {},
-  gridRows = 3,
+  gridRows = 4,
   gridColumns = 4,
   gridCellSize = 64,
   cellMargin = 16,
@@ -21,16 +21,28 @@ end
 function selectionScreen:init(pokemonDirectory)
   local pokemonFileList = love.filesystem.getDirectoryItems(pokemonDirectory)
 
-  for i = 1, #pokemonFileList do
-    local pokemonFile = pokemonFileList[i]
-    -- file names are: <pokemon name>_<type>.png
-    local name, type = getPokemonData(pokemonFile)
+  local pokemonIndex = 1
+  for rowIndex = 1, self.gridRows do
+    for columnIndex = 1, self.gridColumns do
+      if pokemonIndex > #pokemonFileList then break end
 
-    table.insert(self.pokemonItems, {
-      name = name,
-      type = type,
-      image = love.graphics.newImage(pokemonDirectory .. pokemonFile)
-    })
+      local pokemonFile = pokemonFileList[pokemonIndex]
+      local name, type = getPokemonData(pokemonFile)
+
+      -- Precompute grid positions (x, y)
+      local x = (columnIndex - 1) * (self.gridCellSize + self.cellMargin)
+      local y = (rowIndex - 1) * (self.gridCellSize + self.cellMargin)
+
+      table.insert(self.pokemonItems, {
+        name = name,
+        type = type,
+        image = love.graphics.newImage(pokemonDirectory .. pokemonFile),
+        gridX = x,
+        gridY = y
+      })
+
+      pokemonIndex = pokemonIndex + 1
+    end
   end
 
   self.selectionGridWidth = self.gridColumns * (self.gridCellSize + self.cellMargin)
@@ -44,38 +56,19 @@ function selectionScreen:init(pokemonDirectory)
 end
 
 function selectionScreen:draw()
-  -- debug to print name and type of each pokemon
-
-  local pokemonIndex = 1
-  local rowIndex = 1
-  local columnIndex = 1
-
-  for i = 1, #self.pokemonItems do
-    local pokemon = self.pokemonItems[i]
-
-    if rowIndex > self.gridRows then
-      rowIndex = 1
-      columnIndex = columnIndex + 1
-    end
-
-    if columnIndex > self.gridColumns then
-      columnIndex = 1
-      rowIndex = rowIndex + 1
-    end
-
-    local x = (columnIndex - 1) * (self.gridCellSize + self.cellMargin)
-    local y = (rowIndex - 1) * (self.gridCellSize + self.cellMargin)
-
-    local imgX = x + self.gridCellSize / 2
-    local imgY = y + self.gridCellSize / 2
+  for i, pokemon in ipairs(self.pokemonItems) do
+    local imgX = pokemon.gridX + self.gridCellSize / 2
+    local imgY = pokemon.gridY + self.gridCellSize / 2
 
     love.graphics.setColor(colors.white)
 
-    if rowIndex == self.selectedPokemon.y and columnIndex == self.selectedPokemon.x then
+    -- Highlight selected Pokémon
+    if self.selectedPokemon.x == (pokemon.gridX / (self.gridCellSize + self.cellMargin)) + 1 and
+       self.selectedPokemon.y == (pokemon.gridY / (self.gridCellSize + self.cellMargin)) + 1 then
       love.graphics.setColor(colors.mossGreen)
       love.graphics.rectangle("fill",
-        self.selectionGridX + x,
-        self.selectionGridY + y,
+        self.selectionGridX + pokemon.gridX,
+        self.selectionGridY + pokemon.gridY,
         self.gridCellSize,
         self.gridCellSize,
         4
@@ -95,19 +88,17 @@ function selectionScreen:draw()
     local pokemonName = pokemon.name
     love.graphics.print(
       pokemonName,
-      math.floor(self.selectionGridX + x + self.gridCellSize / 2 - font:getWidth(pokemonName) / 2),
-      math.floor(self.selectionGridY + y + self.gridCellSize - font:getHeight() * 1.2)
+      math.floor(self.selectionGridX + pokemon.gridX + self.gridCellSize / 2 - font:getWidth(pokemonName) / 2),
+      math.floor(self.selectionGridY + pokemon.gridY + self.gridCellSize - font:getHeight() * 1.2)
     )
-
-    rowIndex = rowIndex + 1
   end
 
   if self.debug then
-    -- draw red rectangles to show grid
-    for i = 1, self.gridRows do
-      for j = 1, self.gridColumns do
-        local x = (j - 1) * (self.gridCellSize + self.cellMargin)
-        local y = (i - 1) * (self.gridCellSize + self.cellMargin)
+    -- Draw grid for debugging
+    for rowIndex = 1, self.gridRows do
+      for columnIndex = 1, self.gridColumns do
+        local x = (columnIndex - 1) * (self.gridCellSize + self.cellMargin)
+        local y = (rowIndex - 1) * (self.gridCellSize + self.cellMargin)
 
         love.graphics.setColor(colors.brickRed)
         love.graphics.rectangle("line",
